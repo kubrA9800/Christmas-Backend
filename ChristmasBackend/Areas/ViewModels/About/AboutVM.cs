83997b0c0f403fb03954f3134e344afb1f48182d
kubrA9800 <kiubrajm@code.edu.az>
2023-12-03 19:49:34 +0400

    refresh backup

--- ChristmasBackend/Areas/ViewModels/About/AboutVM.cs
+++ ChristmasBackend/Areas/ViewModels/About/AboutVM.cs
@@ -7,5 +7,6 @@
         public string Description { get; set; }
         public string Video { get; set; }
         public string Image { get; set; }
+        public DateTime CreatedDate { get; set; }
     }
 }
